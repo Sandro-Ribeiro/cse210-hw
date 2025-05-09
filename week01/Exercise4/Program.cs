0ffa8f864c69e0774807489ab35d1dfc82224cb4
Sandro Ribeiro <santunesribeiro@outlook.com>
2025-05-09 19:51:37 -0300

    Week 01 - Exercícios 4 Stretch Challenge

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -6,11 +6,14 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Core Requirements
+
         List<int> numbers = new List<int>();
         int number;
         int sum_numbers;
         double average_numbers;
         int max_number;
+        int minPositiveNumber = int.MaxValue;
 
         Console.WriteLine("Enter a list of numbers, type 0 when finished");
 
@@ -35,5 +38,32 @@ class Program
         Console.WriteLine($"The average is: {average_numbers}");
         Console.WriteLine($"The largest number is: {max_number}");
 
+        // Stretch Challenge
+
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            if (numbers[i] > 0 && numbers[i] < minPositiveNumber)
+            {
+                minPositiveNumber = numbers[i];
+            }
+        }
+
+        if (minPositiveNumber != int.MaxValue)
+        {
+            Console.WriteLine($"The smallest positive number is: {minPositiveNumber}");
+        }
+        else
+        {
+            Console.WriteLine("No positive numbers were entered");
+        }
+
+        numbers.Sort();
+
+        Console.WriteLine("The sorted list is: ");
+
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            Console.WriteLine(numbers[i]);
+        }
     }
 }
