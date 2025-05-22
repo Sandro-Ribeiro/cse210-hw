7b0924485cc3290dedca5d56803a0985613286c6
Sandro Ribeiro <santunesribeiro@outlook.com>
2025-05-22 20:50:49 -0300

    Week 03 - Learning Activity: Encapsulation

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,60 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        // Instantiate Fraction objects with no arguments, one argument, and two arguments
+        Fraction fraction_01 = new Fraction();
+        Fraction fraction_02 = new Fraction(6);
+        Fraction fraction_03 = new Fraction(6, 7);
+
+        Console.WriteLine("Getting values of the top and bottom");
+
+        // Displaying the numerator and denominator values of each Fraction object
+        Console.WriteLine("Fraction 01");
+        Console.WriteLine($"{fraction_01.GetTop()}");
+        Console.WriteLine($"{fraction_01.GetBottom()}");
+        Console.WriteLine("Fraction 02");
+        Console.WriteLine($"{fraction_02.GetTop()}");
+        Console.WriteLine($"{fraction_02.GetBottom()}");
+        Console.WriteLine("Fraction 03");
+        Console.WriteLine($"{fraction_03.GetTop()}");
+        Console.WriteLine($"{fraction_03.GetBottom()}");
+
+        // Setting new numerator and denominator values for the first Fraction object
+        fraction_01.SetTop(3);
+        fraction_01.SetBottom(4);
+
+        // Setting new numerator and denominator values for the second Fraction object
+        fraction_02.SetTop(5);
+        fraction_02.SetBottom(8);
+
+        // Setting new numerator and denominator values for the third Fraction object
+        fraction_03.SetTop(1);
+        fraction_03.SetBottom(4);
+
+        Console.WriteLine("Setting new values to top and bottom");
+
+        // Displaying the updated numerator and denominator values of each Fraction object
+        Console.WriteLine("Fraction 01");
+        Console.WriteLine($"{fraction_01.GetTop()}");
+        Console.WriteLine($"{fraction_01.GetBottom()}");
+        Console.WriteLine("Fraction 02");
+        Console.WriteLine($"{fraction_02.GetTop()}");
+        Console.WriteLine($"{fraction_02.GetBottom()}");
+        Console.WriteLine("Fraction 03");
+        Console.WriteLine($"{fraction_03.GetTop()}");
+        Console.WriteLine($"{fraction_03.GetBottom()}");
+
+        Console.WriteLine("Displaying the fractions in string and decimal formats");
+
+        Console.WriteLine("Fraction 01");
+        Console.WriteLine(fraction_01.GetFractionString());
+        Console.WriteLine($"{fraction_01.GetDecimalValue()}");
+        Console.WriteLine("Fraction 02");
+        Console.WriteLine(fraction_02.GetFractionString());
+        Console.WriteLine($"{fraction_02.GetDecimalValue()}");
+        Console.WriteLine("Fraction 03");
+        Console.WriteLine(fraction_03.GetFractionString());
+        Console.WriteLine($"{fraction_03.GetDecimalValue()}");
+
     }
 }
