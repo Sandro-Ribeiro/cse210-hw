1d8e278b70dde3c01b758df18fe456815f840032
Sandro Ribeiro <santunesribeiro@outlook.com>
2025-05-24 03:21:13 -0300

    week 03 - W03 Team Activity: Scripture Memorizer Design - half

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        string book = "Alma";
+        int chapter = 34;
+        int verse = 30;
+        int endVerse = 32;
+        string text = "And now, my brethren, I would that, after ye have received so many witnesses," +
+        " seeing that the holy scriptures testify of these things, ye come forth and bring fruit unto repentance." +
+        " Yea, I would that ye would come forth and harden not your hearts any longer; for behold, now is the time" +
+        " and the day of your salvation; and therefore, if ye will repent and harden not your hearts, immediately" +
+        " shall the great plan of redemption be brought about unto you.For behold, this life is the time for men to" +
+        " prepare to meet God; yea, behold the day of this life is the day for men to perform their labors.";
+
+        Reference reference = new Reference(book, chapter, verse, endVerse);
+        Scripture scripture = new Scripture(reference, text);  
     }
 }
